d2d3d5aeb89cb4132a240d6c91546cd73661c59f
unknown <ronalynmaloloyon0911@gmail.com>
2022-06-10 13:07:26 +0800

    contact tracing but the database cannot add into github

---
+++ ContactTracing/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ContactTracing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection sc = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Charlimagne\source\repos\ContactTracing\ContactTracing\Database1.mdf;Integrated Security=True");
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand sm = new SqlCommand("insert into tracing values('"+textBox1.Text+"','"+textBox2.Text+"','"+textBox3.Text+"','"+textBox4.Text+"','"+textBox5.Text+"','"+textBox6.Text+"','"+textBox7.Text+"','"+textBox8.Text+"')",sc);
+
+            sc.Open();
+
+            sm.ExecuteNonQuery();
+
+
+            sc.Close();
+
+            MessageBox.Show("Your information has been saved successfully!");
+        }
+    }
+}
